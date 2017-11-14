4172f6948450bea215fe9ae3428528cbb6031d8b
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-14 20:14:25 +0100

    Update all todo counters. Move script to an external file

---
+++ script.js
@@ -0,0 +1,22 @@
+  $("#total-count").html($(".todo").length);
+
+  function toggleDone() {
+    var checkbox = this;
+
+    $(checkbox).parent().toggleClass("completed");
+  }
+  $(document).ready(function() {
+    $("input[type=checkbox]").bind('change', toggleDone);
+  });
+  updateCounters();
+
+  function updateCounters() {
+    var todoCount = $(".todo").length;
+    var completedCount = $(".completed").length;
+
+    $("#total-count").html(todoCount);
+    $("#completed-count").html(completedCount);
+    $("#todo-count").html(todoCount - completedCount);
+  }
+
+  updateCounters();
